240a3498ded399030d63074241c85215cd9e281e
JuctTr <1301842163@qq.com>
2022-07-13 22:27:06 +0800

    feat(up): 新增文章

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,7 +1,7 @@
 module.exports = {
     base: '/blog/',
     title: 'JuctTr的博客',
-    description: 'JuctTr的博客',
+    description: '记录自己的学习过程',
     theme: 'reco',
     themeConfig: {
         subSidebar: 'auto',
@@ -17,7 +17,7 @@ module.exports = {
         ],
         sidebar: [
             {
-                title: '欢迎学习',
+                title: '首页',
                 path: '/',
                 collapsable: false, // 不折叠
                 children: [
@@ -25,12 +25,21 @@ module.exports = {
                 ]
             },
             {
-              title: "基础学习",
-              path: '/handbook/type',
+              title: "React",
+              path: '/react/How-do-React-hooks-really-work',
               collapsable: false, // 不折叠
               children: [
-                { title: "条件类型", path: "/handbook/type" },
-                { title: "泛型", path: "/handbook/type" }
+                { title: "React是如何真正工作的", path: "/handbook/How-do-React-hooks-really-work" },
+                // { title: "泛型", path: "/handbook/type" }
+              ],
+            },
+            {
+              title: "ECMAScript",
+              path: '/ecmaScript/type_compare',
+              collapsable: false, // 不折叠
+              children: [
+                { title: "显式类型转换和隐式类型转换", path: "/ecmaScript/type_compare" },
+                // { title: "泛型", path: "/handbook/type" }
               ],
             }
         ],
